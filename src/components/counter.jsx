2e9f0763a0ecf583dcf8782b20b87fad9aebf6a3
민동현 <mindonghyeon890@gmail.com>
2019-08-25 11:48:53 +0900

    add Decrement Button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,21 +4,32 @@ class Counter extends Component {
   render() {
     return (
       // div를 사용하고 싶지 않으면 React.Fragment를 사용하자.
-      <div>
-        <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
-        {/* this.handleIncrement vs this.handleIncrement ()? */}
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"
-        >
-          Delete
-        </button>
+      <div className="row">
+        <div className="co1-1">
+          <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+        </div>
+        <div className="col">
+          {/* this.handleIncrement vs this.handleIncrement ()? */}
+          <button
+            onClick={() => this.props.onIncrement(this.props.counter)}
+            className="btn btn-secondary btn-sm"
+          >
+            +
+          </button>
+          <button
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0 ? "disabled" : ""}
+          >
+            -
+          </button>
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     );
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Counter from "./counter";
 class counters extends Component {
   render() {
-    const { onReset, counters, onDelete, onIncrement } = this.props;
+    const { onReset, counters, onDelete, onIncrement, onDecrement } = this.props;
     return (
       <div>
         <button onClick={onReset} className="btn btn-primary btn-sm m-2">
@@ -14,6 +14,7 @@ class counters extends Component {
             key={counter.id}
             onDelete={onDelete}
             onIncrement={onIncrement}
+            onDecrement={onDecrement}
             counter={counter}
           />
         ))}
